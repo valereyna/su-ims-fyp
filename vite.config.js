b0e4e1ceacceab285863a83f75cc628f1edd826a
valereyna <145428872+valereyna@users.noreply.github.com>
2025-04-30 15:57:37 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+          external: ['alpinejs']
+        }
+    },
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
